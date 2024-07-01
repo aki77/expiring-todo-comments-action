0b237a9e44829cec50b95c1dc8cab94032a863e6
aki <lala.akira@gmail.com>
2024-07-02 07:21:33 +0900

    chore: Fix UTC date conversion in formatDate function

--- dist/index.js
+++ dist/index.js
@@ -277,9 +277,9 @@ const isComment = (file, text) => {
 };
 exports.isComment = isComment;
 const formatDate = (date) => {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
+    const year = date.getUTCFullYear();
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+    const day = String(date.getUTCDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
 };
 exports.formatDate = formatDate;
